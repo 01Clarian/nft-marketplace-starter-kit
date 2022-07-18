662da2332a324af1d6d38562c7574623b966a220
Aviroxi <gurugkgurukiran@gmail.com>
2022-07-18 12:43:34 +0530

    initial configuration working

---
+++ src/contracts/Nft.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Nft{
+    string public name;
+    string public symbol;
+
+    constructor(){
+        name='NFT smart cards';
+        symbol='NFTz';
+    }
+}
